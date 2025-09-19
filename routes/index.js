27dd14611547a7de2118690de1aee3fb42118bbd
MANDALORE\nilsg <test@gmail.com>
2025-09-19 14:14:37 +0200

    add player page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 // Standardseite
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Spielerseite
+router.get('/player', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/player.html'));
 });
 
 module.exports = router;
